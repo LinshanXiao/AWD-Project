94167156b8a4b66a542e910a8d1199d316a5bc70
AfricanAlien <dirk.haasbroek7@gmail.com>
2025-04-12 21:12:12 +0800

    Created initial framework for running flask website

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from app.models import db
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+from app import routes
