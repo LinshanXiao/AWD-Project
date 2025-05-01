6cc328ca5dcedc408bf241d8ddc0346733a4a3ef
AfricanAlien <dirk.haasbroek7@gmail.com>
2025-05-02 07:09:55 +0800

    Updated database models to comply with newer SQLAlchemy 2.0 syntax

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,15 @@
 from flask import Flask
-from app.models import db
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'our_secret_key_here'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-
-db.init_app(app)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 from app import routes, models
--- app/models.py
+++ app/models.py
@@ -1,35 +1,47 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import db
 
 class User(db.Model):
-    username = db.Column(db.String(150), nullable=False, unique=True, primary_key=True)
-    password = db.Column(db.String(150), nullable=False)
-    email = db.Column(db.String(150), nullable=False, unique=True)
-    league_username = db.Column(db.String(150), nullable=True, unique=True)
-    valorant_username = db.Column(db.String(150), nullable=True, unique=True)
-    PUBG_username = db.Column(db.String(150), nullable=True, unique=True)
-    apex_username = db.Column(db.String(150), nullable=True, unique=True)
+    __tablename__ = "user"
+    id : so.Mapped[int] = so.mapped_column(primary_key=True, autoincrement=True)
+    username: so.Mapped[str] = so.mapped_column(sa.String(150), unique=True, nullable=False)
+    password: so.Mapped[str] = so.mapped_column(sa.String(150), nullable=False)
+    email: so.Mapped[str] = so.mapped_column(sa.String(150), unique=True, nullable=False)
+    league_username: so.Mapped[str | None] = so.mapped_column(sa.String(150), unique=True, nullable=True)
+    valorant_username: so.Mapped[str | None] = so.mapped_column(sa.String(150), unique=True, nullable=True)
+    PUBG_username: so.Mapped[str | None] = so.mapped_column(sa.String(150), unique=True, nullable=True)
+    apex_username: so.Mapped[str | None] = so.mapped_column(sa.String(150), unique=True, nullable=True)
+
+    league_games: so.WriteOnlyMapped["list[League_Game_Instance]"] = so.relationship(
+        back_populates="users", secondary="league_game_player", lazy="dynamic"
+    )
 
 class League_Game_Instance(db.Model):
-    game_id = db.Column(db.Integer, primary_key=True)
-    date_played = db.Column(db.DateTime, nullable=False)
-    game_duration = db.Column(db.Time, nullable=False)
-    winning_team = db.Column(db.String(150), nullable=False)
+    __tablename__ = "league_game_instance"
+
+    game_id: so.Mapped[int] = so.mapped_column(primary_key=True, autoincrement=True)
+    date_played: so.Mapped[sa.DateTime] = so.mapped_column(nullable=False)
+    game_duration: so.Mapped[sa.Time] = so.mapped_column(nullable=False)
+    winning_team: so.Mapped[str] = so.mapped_column(sa.String(150), nullable=False)
+
+    users: so.WriteOnlyMapped["list[User]"] = so.relationship(
+        back_populates="league_games", secondary="league_game_player"
+    )
 
 class League_Game_Player(db.Model):
-    league_username = db.Column(db.String(150), db.ForeignKey('user.league_username'), primary_key=True)
-    game_id = db.Column(db.Integer, db.ForeignKey('league_game_instance.game_id'), primary_key=True)
-    champion = db.Column(db.String(150), nullable=False)
-    kills = db.Column(db.Integer, nullable=False)
-    deaths = db.Column(db.Integer, nullable=False)
-    assists = db.Column(db.Integer, nullable=False)
-    kda = db.Column(db.Float, nullable=False)
-    team = db.Column(db.String(150), nullable=False)
+    __tablename__ = "league_game_player"
 
-User.league_games = db.relationship(
-    'League_Game_Instance',
-    secondary=League_Game_Player.__table__,
-    backref='user',
-    lazy='dynamic'
-)
+    league_username: so.Mapped[str] = so.mapped_column(
+        sa.String(150), sa.ForeignKey("user.league_username"), primary_key=True
+    )
+    game_id: so.Mapped[int] = so.mapped_column(
+        sa.Integer, sa.ForeignKey("league_game_instance.game_id"), primary_key=True
+    )
+    champion: so.Mapped[str] = so.mapped_column(sa.String(150), nullable=False)
+    kills: so.Mapped[int] = so.mapped_column(nullable=False)
+    deaths: so.Mapped[int] = so.mapped_column(nullable=False)
+    assists: so.Mapped[int] = so.mapped_column(nullable=False)
+    kda: so.Mapped[float] = so.mapped_column(nullable=False)
+    team: so.Mapped[str] = so.mapped_column(sa.String(150), nullable=False)
+
