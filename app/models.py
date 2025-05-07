fb33ce034570c5ef9e9d8665422e94b2fc055e4f
LinshanXiao <23600251@student.uwa.edu.au>
2025-05-08 00:02:36 +0800

    added a database for friendship to achieve basic user friend system

--- app/models.py
+++ app/models.py
@@ -47,3 +47,13 @@ class League_Game_Player(db.Model):
     kda: so.Mapped[float] = so.mapped_column(nullable=False)
     team: so.Mapped[str] = so.mapped_column(sa.String(150), nullable=False)
 
+class Friendship(db.Model):
+    __tablename__ = "friendship"
+
+    id: so.Mapped[int] = so.mapped_column(primary_key=True, autoincrement=True)
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey('user.id'), nullable=False)
+    friend_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey('user.id'), nullable=False)
+
+    __table_args__ = (
+        sa.UniqueConstraint('user_id', 'friend_id', name='unique_friendship'),
+    )
